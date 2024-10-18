6dd200621270dbabeba75f7a5ac67f355c350908
gabbi 🎀 <agabflix@gmail.com>
2024-10-18 11:26:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var count = 0;
+
+var output = document.getElementById('output');
+
+function countClicks()
+
+{
+count = count +1;
+output.innerHTML= count; 
+}
+
+fita.addEventListener('mouseover', () => {
+    imagem .style.transf    
+}) 
